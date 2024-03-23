d60a5b8f97456f1eb6d52ace8fae697d86e59c70
Anastasiia Hopchuk <anastasiiahopch@gmail.com>
2024-03-23 08:34:12 +0100

    feat: add responsive navbar

---
+++ js/sidenav.js
@@ -0,0 +1,19 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "100%";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+
+function checkScreenWidth() {
+    if (window.matchMedia("(min-width: 480px)").matches) {
+        openNav(); // Call your function
+    } else closeNav();
+}
+
+// Initial check when the page loads
+checkScreenWidth();
+
+// Listen for resize events and check screen width again
+window.addEventListener('resize', checkScreenWidth);
