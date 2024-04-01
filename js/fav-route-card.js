f36f79b7038e8796061cf2a583f01d049903e10a
Anastasiia Hopchuk <anastasiiahopch@gmail.com>
2024-04-01 09:20:43 +0200

    feat: save info about likes in JSON format

--- js/fav-route-card.js
+++ js/fav-route-card.js
@@ -1,45 +1,61 @@
 // Loop through tabRecords and generate HTML for each section
 const favSectionsContainer = document.getElementById('fav-routes');
-if (sessionStorage.length) {
-    tabRecords.forEach((record, index) => {
-        if (sessionStorage.getItem(`${record.id}-like-btn`) !== null) {
-            const sectionHTML = `
-        <article class="article-section">
-            <img src="${record.img}" alt=${record.alt}"">
-            <div class="section-content">
-                <div class="section-heading">
-                    <h2>${record.name}</h2>
-                    <button id="${record.id}-like-btn" class="liked like-btn">
-                        <svg viewBox="0 0 24 24"
-                            fill="none" stroke-linecap="round" stroke-linejoin="round">
-                            <path  d="M19.5 12.572l-7.5 7.428l-7.5 -7.428a5 5 0 1 1 7.5 -6.566a5 5 0 1 1 7.5 6.572"/>
-                        </svg>
-                    </button>
-                </div>
-                <strong>Start:</strong> ${record.start}
-                <br><strong>End:</strong> ${record.end}
-                <br><strong>Distance:</strong> ${record.distance}
-                <br>${record.about}
-                
-            </div>
-            <button class="more-info-btn" onclick="generateMoreInfo('${record.id}')">More</button>
-        </article>
-        `;
-
-            favSectionsContainer.innerHTML += sectionHTML;
+let likes = JSON.parse(sessionStorage.getItem('likes'));
+if (likes !==null && likes.length>0) {
+    for (let i = 0; i < likes.length; i++) {
+        for (let j = 0; j < tabRecords.length; j++) {
+            if (likes[i]===tabRecords[j].id)
+            {
+                let record = tabRecords[j];
+                let sectionHTML = `
+                <article class="article-section">
+                    <img src="${record.img}" alt=${record.alt}"">
+                    <div class="section-content">
+                        <div class="section-heading">
+                            <h2>${record.name}</h2>
+                            <button id="${record.id}-like-btn" class="liked like-btn" onclick="unlike('${record.id}')">
+                                <svg viewBox="0 0 24 24"
+                                    fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                    <path  d="M19.5 12.572l-7.5 7.428l-7.5 -7.428a5 5 0 1 1 7.5 -6.566a5 5 0 1 1 7.5 6.572"/>
+                                </svg>
+                            </button>
+                        </div>
+                        <strong>Start:</strong> ${record.start}
+                        <br><strong>End:</strong> ${record.end}
+                        <br><strong>Distance:</strong> ${record.distance}
+                        <br>${record.about}
+                        
+                    </div>
+                    <button class="more-info-btn" onclick="generateMoreInfo('${record.id}')">More</button>
+                </article>
+                `;
+                favSectionsContainer.innerHTML += sectionHTML;
+            }
         }
-    });
+    }
 } else {
     favSectionsContainer.innerHTML = `<article id="nothing-is-here"><b>Nothing is here</b><br>Try to like some routes in All routes tab<div><button id="all-routes-btn" onclick="navigateToPage('all-routes.html')">All routes</button></div></article>`
 }
 
-const likeButtons = document.querySelectorAll(".like-btn");
+// const likeButtons = document.querySelectorAll(".like-btn");
 
-likeButtons.forEach((likeButton) => {
-    likeButton.addEventListener("click", () => {
-        sessionStorage.removeItem(likeButton.id);
-        // Reload the current page
-        location.reload();
+// likeButtons.forEach((likeButton) => {
+//     likeButton.addEventListener("click", () => {
+//         sessionStorage.removeItem(likeButton.id);
+//
+//
+//     });
+// });
 
-    });
-});
+function unlike(recordId){
+    let likes = JSON.parse(sessionStorage.getItem('likes'));
+    let i;
+    for (i = 0; i < likes.length; i++) {
+        if (likes[i] === recordId) break;
+    }
+    likes.splice(i,1);
+    sessionStorage.setItem('likes', JSON.stringify(likes));
+
+    // Reload the current page
+    location.reload();
+}
--- js/route-card.js
+++ js/route-card.js
@@ -8,7 +8,7 @@ tabRecords.forEach((record, index) => {
             <div class="section-content">
                 <div class="section-heading">
                     <h2>${record.name}</h2>
-                    <button id="${record.id}-like-btn" class="like-btn">
+                    <button id="${record.id}-like-btn" class="like-btn" onclick='like("${record.id}")'>
                         <svg viewBox="0 0 24 24"
                             fill="none" stroke-linecap="round" stroke-linejoin="round">
                             <path  d="M19.5 12.572l-7.5 7.428l-7.5 -7.428a5 5 0 1 1 7.5 -6.566a5 5 0 1 1 7.5 6.572"/>
@@ -31,12 +31,15 @@ tabRecords.forEach((record, index) => {
 
 // Function to add "liked" class to buttons if they were previously liked
 function addLikedClassToButtons() {
-    tabRecords.forEach((record) => {
-        const button = document.getElementById(`${record.id}-like-btn`);
-        if (sessionStorage.getItem(`${record.id}-like-btn`) !== null && button !== null) {
-            button.classList.add("liked");
-        }
-    });
+    let likes = JSON.parse(sessionStorage.getItem('likes'));
+    if (likes) {
+        likes.forEach((like) => {
+            let button = document.getElementById(`${like}-like-btn`);
+            if (button !== null) {
+                button.classList.add("liked");
+            }
+        });
+    }
 }
 
 // Call the function after appending the elements
@@ -44,12 +47,47 @@ addLikedClassToButtons();
 
 const likeButtons = document.querySelectorAll(".like-btn");
 
-likeButtons.forEach((likeButton) => {
-    likeButton.addEventListener("click", () => {
-        if (likeButton.classList.toggle("liked"))
-            sessionStorage.setItem(likeButton.id, 'like');
-        else sessionStorage.removeItem(likeButton.id);
-    });
-});
+function like(recordId){
+    let likes = JSON.parse(sessionStorage.getItem('likes'));
+    if (likes === null) likes = []; //pracujemy z tablicą obiektów
+    let likeButton = document.getElementById(`${recordId}-like-btn`);
+    if (likeButton.classList.toggle("liked")) {
+        likes.push(recordId); //dodaj nowy obiekt do listy
+    } else {
+        let i;
+        for (i = 0; i < likes.length; i++) {
+            if (likes[i] === recordId) break;
+        }
+        likes.splice(i,1);
+    }
+    sessionStorage.setItem('likes', JSON.stringify(likes));
+}
 
-
+// function readAll() {
+//     let likes = JSON.parse(localStorage.getItem('likes'));
+//     if (likes!=null) {
+//         let data = "";
+//         for (let i = 0; i < likes.length; i++) {
+//             data += "<tr>" +
+//                 "<td><button onclick='deleteItem("+i+")'> X </button></td>" +
+//                 "<td>" + items[i].name + "</td>" +
+//                 "<td>" + items[i].price + "</td>" +
+//                 "<td>" + items[i].color + "</td>" +
+//                 "<td>" + items[i].quantity + "</td>" +
+//                 "</tr>";
+//         }
+//         data += "</table>";
+//         basketData.innerHTML = data;
+//     }
+//     else {
+//         basketData.innerHTML = "<h3>Koszyk jest pusty</h3>";
+//     }
+// }
+//
+// function deleteItem(i) {
+//     let likes = JSON.parse(localStorage.getItem('liked'));
+//     //usuń i-ty element z listy zadań:
+//     likes.splice(i,1);
+//     //zapisz zaktualizowaną listę w localStorage:
+//     localStorage.setItem('liked', JSON.stringify(likes)); //zapisz listę
+// }
